2f8d79ce8ebdbe1ab750fe42ab244c131f8824f7
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2021-04-10 03:50:49 +0700

    :zap: Removed constants.dart ( since I didn't know yet )

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,55 @@
 import 'package:flutter/material.dart';
-// import './constants.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    var questions = ["What's your spirit animal ?", "What's your age ?"];
+
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-                title: Text('Kuquiz'),
-            ),
-            body: Column(
-                children: [
-                  Text('Question!'),
-                  RaisedButton(
-                      child: Text('Answer 1!'),
-                      onPressed: null,
-                  ),
-                  RaisedButton(
-                      child: Text('Answer 2!'),
-                      onPressed: null,
-                  ),
-                  RaisedButton(
-                      child: Text('Answer 3!'),
-                      onPressed: null,
-                  ),
-                ],
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Kuquiz'),
         ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1!'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2!'),
+              onPressed: () => print('answer 2 gemink'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3!'),
+              onPressed: () {
+                var total = 2 + 3;
+
+                print(total);
+              },
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex += 1;
+    });
+    print(questionIndex);
   }
 }
